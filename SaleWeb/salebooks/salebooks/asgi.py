cac3b8bacb9175d874c2af8c221ccf9e3e9b9432
huynhchuongne <108483893+huynhchuongne@users.noreply.github.com>
2024-09-10 05:47:07 +0700

    add

---
+++ SaleWeb/salebooks/salebooks/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for salebooks project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salebooks.settings')
+
+application = get_asgi_application()
