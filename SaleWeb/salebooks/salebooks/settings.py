c5d35fa476524cb34971584e8e725cc025a2811a
huynhchuongne <108483893+huynhchuongne@users.noreply.github.com>
2024-09-10 05:52:45 +0700

    add-database

--- SaleWeb/salebooks/salebooks/settings.py
+++ SaleWeb/salebooks/salebooks/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-qwb97m7v#bbzv@m6)%=9k8yc!w*erfs_ubp50sxnge_qm6p(lr
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,17 +68,31 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'salebooks.wsgi.application'
 
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'salebooks',
+        'USER': 'root',
+        'PASSWORD': 'chuongne123',
+        'HOST': ''  # mặc định localhost
     }
+
 }
 
+import cloudinary
+
+cloudinary.config(
+    cloud_name="dwcsz7tho",
+    api_key="599197224987993",
+    api_secret="hguWwepEzS-ICH2xna9YI1iGt4M"
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -100,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -111,7 +122,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
